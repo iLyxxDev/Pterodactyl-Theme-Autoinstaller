9bb24059fd61b73c2e3ab7fe436447ec26a8b516
iLyxxDev <kprlbdt@gmail.com>
2025-08-12 02:07:41 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -343,7 +343,7 @@ cd /var/www/pterodactyl || { echo "Direktori tidak ditemukan"; exit 1; }
 # Membuat lokasi baru
 php artisan p:user:make <<EOF
 yes
-naeladmm@gmail.com
+adminnnael@gmail.com
 $user
 $user
 $user
